11443c0a5ffcfa4b7fbd99a985fee25b56cb443e
Andrew Whitehead <cywolf@gmail.com>
2021-08-20 20:46:29 -0700

    add token creation, verification
    Signed-off-by: Andrew Whitehead <cywolf@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,23 @@
 use std::time::Instant;
 
-use bls12_381::*;
+use blake2::{
+    crypto_mac::generic_array::{typenum::Unsigned, GenericArray},
+    digest::{Update, VariableOutput},
+    Blake2b, VarBlake2b,
+};
+use bls12_381::{hash_to_curve::*, *};
 use ff::Field;
 use group::{Curve, Wnaf};
 use rand::rngs::OsRng;
 
-pub fn isser_create_accum(sk: &Scalar, members: &[Scalar]) -> G1Projective {
-    isser_update_accum(sk, G1Projective::generator(), members, true)
+const G2_UNCOMPRESSED_SIZE: usize = 192;
+
+pub fn isser_create_accum(rk: &Scalar, members: &[Scalar]) -> G1Projective {
+    isser_update_accum(rk, G1Projective::generator(), members, true)
 }
 
 pub fn isser_update_accum(
-    sk: &Scalar,
+    rk: &Scalar,
     base: G1Projective,
     members: &[Scalar],
     add: bool,
@@ -18,9 +25,9 @@ pub fn isser_update_accum(
     if members.is_empty() {
         base
     } else {
-        let mut acc = members[0] + sk;
+        let mut acc = members[0] + rk;
         for x in &members[1..] {
-            acc *= x + sk;
+            acc *= x + rk;
         }
         if !add {
             acc = acc.invert().unwrap();
@@ -66,12 +73,88 @@ pub fn prover_combine_witness(
     wit.to_affine()
 }
 
+fn hash_to_g1(input: &[u8]) -> G1Projective {
+    const DST: &[u8] = b"bbs-registry;v=1";
+    <G1Projective as HashToCurve<ExpandMsgXmd<Blake2b>>>::hash_to_curve(input, DST)
+}
+
+fn create_generators(pk: &G2Affine, h: &G2Affine) -> (G1Projective, G1Projective) {
+    let mut buf = [0u8; G2_UNCOMPRESSED_SIZE * 2 + 2 + 4];
+    buf[..G2_UNCOMPRESSED_SIZE].copy_from_slice(&pk.to_uncompressed()[..]);
+    buf[G2_UNCOMPRESSED_SIZE + 1..(G2_UNCOMPRESSED_SIZE * 2 + 1)]
+        .copy_from_slice(&pk.to_uncompressed());
+    buf[G2_UNCOMPRESSED_SIZE + 1..(G2_UNCOMPRESSED_SIZE * 2 + 1)]
+        .copy_from_slice(&h.to_uncompressed());
+    let h0 = hash_to_g1(&buf);
+    buf[G2_UNCOMPRESSED_SIZE * 2 + 2..].copy_from_slice(&1u32.to_be_bytes());
+    let h1 = hash_to_g1(&buf);
+    (h0, h1)
+}
+
+pub struct Token {
+    pk: G2Affine,
+    h: G2Affine,
+    timestamp: u64,
+    accum: G1Affine,
+    witness: G1Affine,
+    sig: G1Affine,
+    e: Scalar,
+}
+
+impl Token {
+    pub fn new(
+        sk: &Scalar,
+        pk: &G2Affine,
+        h: &G2Affine,
+        q: Scalar,
+        timestamp: u64,
+        accum: &G1Affine,
+        witness: &G1Affine,
+    ) -> Self {
+        let (h0, h1) = create_generators(pk, h);
+        let e = Scalar::random(OsRng); // will be deterministic
+        let b = h0 * q + h1 * Scalar::from(timestamp) + accum;
+        let sig = (b * (sk + e).invert().unwrap()).to_affine();
+        Self {
+            pk: *pk,
+            h: *h,
+            timestamp,
+            accum: *accum,
+            witness: *witness,
+            sig,
+            e,
+        }
+    }
+
+    pub fn create_q(reg_id: &str, block: u32) -> Scalar {
+        let mut hash_q = VarBlake2b::new(<Scalar as HashToField>::InputLength::USIZE).unwrap();
+        hash_q.update(reg_id);
+        hash_q.update(b"0");
+        hash_q.update(block.to_be_bytes());
+        let mut buf = GenericArray::default();
+        hash_q.finalize_variable(|hash| {
+            buf.copy_from_slice(hash);
+        });
+        Scalar::from_okm(&buf)
+    }
+
+    pub fn verify(&self, q: Scalar, member: Scalar) -> bool {
+        let (h0, h1) = create_generators(&self.pk, &self.h);
+        let b = h0 * q + h1 * Scalar::from(self.timestamp) + self.accum;
+        pairing(
+            &self.sig,
+            &(G2Projective::generator() * self.e + self.pk).to_affine(),
+        ) == pairing(&b.to_affine(), &G2Affine::generator())
+            && check_witness(member, &self.witness, &self.accum, &self.h)
+    }
+}
+
 fn main() {
     let size = 1024;
     let members: Vec<_> = (0..size).map(|_| Scalar::random(OsRng)).collect();
-    let sk = Scalar::random(OsRng);
+    let rk = Scalar::random(OsRng);
     let start = Instant::now();
-    let acc = isser_create_accum(&sk, &members);
+    let acc = isser_create_accum(&rk, &members);
     println!("create accum: {:?}", start.elapsed());
 
     let start = Instant::now();
@@ -79,14 +162,14 @@ fn main() {
     let mut acc_wnaf = wnaf.base(acc, 4);
     let witness_proj: Vec<_> = members
         .iter()
-        .map(|m| acc_wnaf.scalar(&(m + sk).invert().unwrap()))
+        .map(|m| acc_wnaf.scalar(&(m + rk).invert().unwrap()))
         .collect();
     let mut witness = vec![G1Affine::identity(); size];
     G1Projective::batch_normalize(&witness_proj, &mut witness);
     println!("create witnesses: {:?}", start.elapsed());
 
     let acc = acc.to_affine();
-    let h = (G2Projective::generator() * sk).to_affine();
+    let h = (G2Projective::generator() * rk).to_affine();
 
     let start = Instant::now();
     for (mem, wit) in members.iter().zip(&witness) {
@@ -96,7 +179,7 @@ fn main() {
 
     let start = Instant::now();
     let rem_from = size / 2;
-    let acc1 = isser_update_accum(&sk, acc.into(), &members[rem_from..], false).to_affine();
+    let acc1 = isser_update_accum(&rk, acc.into(), &members[rem_from..], false).to_affine();
     println!("update accum: {:?}", start.elapsed());
 
     let start = Instant::now();
@@ -106,12 +189,22 @@ fn main() {
     let wit = prover_combine_witness(&members, &witness, &removed[..]);
     println!("combine witness: {:?}", start.elapsed());
     // members[0] is in the accum but not the witness:
-    // specifically, accum = witness * (members[0] + sk)
+    // specifically, accum = witness * (members[0] + rk)
     assert!(check_witness(members[0], &wit, &acc1, &h));
     // members[1] is in both the accum and the witness
     assert!(!check_witness(members[1], &acc1, &wit, &h));
     // members[rem_from] is not in the accum or the witness
     assert!(!check_witness(members[rem_from], &wit, &acc1, &h));
+
+    let sk = Scalar::random(OsRng);
+    let pk = (G2Projective::generator() * sk).to_affine();
+    let reg_id = "registry-id";
+    let block = 10001;
+    let timestamp = 9992595252;
+    let q = Token::create_q(reg_id, block);
+    let token = Token::new(&sk, &pk, &h, q, timestamp, &acc1, &wit);
+    assert!(token.verify(q, members[0]));
+    println!("done");
 }
 
 #[test]
