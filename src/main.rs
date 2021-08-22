af2c2c11f192183f5d3f2ffa83e5d831b4f303df
Andrew Whitehead <cywolf@gmail.com>
2021-08-21 17:00:54 -0700

    add zero-knowledge proof
    Signed-off-by: Andrew Whitehead <cywolf@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -78,6 +78,7 @@ fn hash_to_g1(input: &[u8]) -> G1Projective {
     <G1Projective as HashToCurve<ExpandMsgXmd<Blake2b>>>::hash_to_curve(input, DST)
 }
 
+#[derive(Clone, Copy, Debug)]
 pub struct Generators {
     pk: G2Affine,
     h: G2Affine,
@@ -105,6 +106,7 @@ impl Generators {
     }
 }
 
+#[derive(Clone, Copy, Debug)]
 pub struct Signature {
     a: G1Affine,
     e: Scalar,
@@ -120,22 +122,32 @@ impl Signature {
     ) -> Self {
         // TODO - make deterministic
         let e = Scalar::random(OsRng);
-        // TODO - sum-of-products
-        let b = gens.h0 * q + gens.h1 * Scalar::from(timestamp) + accum;
+        let b = Self::calc_b(gens, q, timestamp, accum);
         let a = (b * (sk + e).invert().unwrap()).to_affine();
         Self { a, e }
     }
 
     pub fn verify(&self, gens: &Generators, q: Scalar, timestamp: u64, accum: &G1Affine) -> bool {
-        // TODO - sum-of-products
-        let b = gens.h0 * q + gens.h1 * Scalar::from(timestamp) + accum;
+        let b = Self::calc_b(gens, q, timestamp, accum);
         pairing(
             &self.a,
             &(G2Projective::generator() * self.e + gens.pk).to_affine(),
         ) == pairing(&b.to_affine(), &G2Affine::generator())
     }
+
+    #[inline]
+    pub(crate) fn calc_b(
+        gens: &Generators,
+        q: Scalar,
+        timestamp: u64,
+        accum: &G1Affine,
+    ) -> G1Projective {
+        // TODO - sum-of-products
+        gens.h0 * q + gens.h1 * Scalar::from(timestamp) + accum
+    }
 }
 
+#[derive(Clone, Copy, Debug)]
 pub struct Token {
     pk: G2Affine,
     h: G2Affine,
@@ -164,21 +176,203 @@ impl Token {
     }
 
     pub fn create_q(reg_id: &str, block: u32) -> Scalar {
-        let mut hash_q = VarBlake2b::new(<Scalar as HashToField>::InputLength::USIZE).unwrap();
+        let mut hash_q = HashScalar::new();
         hash_q.update(reg_id);
         hash_q.update(b"0");
         hash_q.update(block.to_be_bytes());
+        hash_q.finalize()
+    }
+
+    pub fn generators(&self) -> Generators {
+        Generators::new(&self.pk, &self.h)
+    }
+
+    pub fn verify(&self, q: Scalar, m: Scalar) -> bool {
+        let gens = self.generators();
+        self.sig.verify(&gens, q, self.timestamp, &self.accum)
+            && check_witness(m, &self.witness, &self.accum, &self.h)
+    }
+
+    pub fn prepare_proof(&self, q: Scalar, m: Scalar) -> PreparedProof {
+        let gens = self.generators();
+        let r1 = Scalar::random(OsRng); // could ensure not zero
+        let r2 = r1.invert().unwrap();
+        let b = Signature::calc_b(&gens, q, self.timestamp, &self.accum);
+
+        // revealed
+        let a_prime = self.sig.a * r1;
+        let w_prime = self.witness * r1;
+        let w_prime_m = w_prime * m;
+        let b_r1 = b * r1;
+        let d = b_r1 - w_prime_m;
+        let a_bar = b_r1 - (a_prime * self.sig.e);
+        let w_bar = self.accum * r1 - w_prime_m;
+
+        // blindings
+        let e_b = Scalar::random(OsRng);
+        let q_b = Scalar::random(OsRng);
+        let m_b = Scalar::random(OsRng);
+        let r2_b = Scalar::random(OsRng);
+
+        // commitments
+        let c1 = a_prime * e_b + w_prime * m_b;
+        let c2 = (d - w_bar) * r2_b - gens.h0 * q_b;
+
+        // TODO - batch normalize
+        PreparedProof {
+            c_vals: ChallengeValues {
+                a_prime: a_prime.into(),
+                a_bar: a_bar.into(),
+                w_prime: w_prime.into(),
+                w_bar: w_bar.into(),
+                d: d.into(),
+            },
+            c1: c1.into(),
+            c2: c2.into(),
+            e_b,
+            q_b,
+            m_b,
+            r2_b,
+            e: self.sig.e,
+            q,
+            m,
+            r2,
+        }
+    }
+}
+
+#[derive(Clone, Copy, Debug)]
+struct ChallengeValues {
+    a_prime: G1Affine,
+    a_bar: G1Affine,
+    w_prime: G1Affine,
+    w_bar: G1Affine,
+    d: G1Affine,
+}
+
+impl ChallengeValues {
+    // TODO challenge would never be used on its own, only write the bytes somewhere
+    pub fn hash(&self, c1: &G1Affine, c2: &G1Affine) -> Scalar {
+        let mut hash_c = HashScalar::new();
+        hash_c.update(&self.a_prime.to_uncompressed());
+        hash_c.update(&self.a_bar.to_uncompressed());
+        hash_c.update(&self.w_prime.to_uncompressed());
+        hash_c.update(&self.w_bar.to_uncompressed());
+        hash_c.update(&self.d.to_uncompressed());
+        hash_c.update(&c1.to_uncompressed());
+        hash_c.update(&c2.to_uncompressed());
+        hash_c.finalize()
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct PreparedProof {
+    c_vals: ChallengeValues,
+    c1: G1Affine,
+    c2: G1Affine,
+    e_b: Scalar,
+    q_b: Scalar,
+    m_b: Scalar,
+    r2_b: Scalar,
+    // not shared:
+    e: Scalar,
+    q: Scalar,
+    m: Scalar,
+    r2: Scalar,
+}
+
+impl PreparedProof {
+    // TODO challenge would never be used on its own, only write bytes somewhere
+    pub fn create_challenge(&self) -> Scalar {
+        self.c_vals.hash(&self.c1, &self.c2)
+    }
+
+    pub fn complete(&self, c: Scalar) -> MembershipProof {
+        let e_hat = self.e_b + c * self.e;
+        let q_hat = self.q_b - c * self.q;
+        let m_hat = self.m_b - c * self.m;
+        let r2_hat = self.r2_b - c * self.r2;
+        MembershipProof {
+            c_vals: self.c_vals,
+            e_hat,
+            q_hat,
+            m_hat,
+            r2_hat,
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct MembershipProof {
+    c_vals: ChallengeValues,
+    e_hat: Scalar,
+    q_hat: Scalar,
+    m_hat: Scalar,
+    r2_hat: Scalar,
+}
+
+impl MembershipProof {
+    // TODO challenge would never be used on its own, only write bytes somewhere
+    pub fn create_challenge(&self, gens: &Generators, c: Scalar, timestamp: u64) -> Scalar {
+        let ChallengeValues {
+            a_prime,
+            a_bar,
+            w_prime,
+            w_bar,
+            d,
+        } = self.c_vals;
+        let c1 = a_prime * self.e_hat + w_prime * self.m_hat + (G1Projective::from(a_bar) - d) * c;
+        let c2 = (G1Projective::from(d) - w_bar) * self.r2_hat - gens.h0 * self.q_hat
+            + (gens.h1 * Scalar::from(timestamp) * c);
+        // TODO batch normalize
+        let (c1, c2) = (c1.to_affine(), c2.to_affine());
+        self.c_vals.hash(&c1, &c2)
+    }
+
+    pub fn verify(&self, pk: &G2Affine, h: &G2Affine) -> bool {
+        let ChallengeValues {
+            a_prime,
+            a_bar,
+            w_prime,
+            w_bar,
+            ..
+        } = self.c_vals;
+        let pair_a = pairing(&a_prime, pk) == pairing(&a_bar, &G2Affine::generator());
+        let pair_w = pairing(&w_prime, h) == pairing(&w_bar, &G2Affine::generator());
+        pair_a && pair_w
+    }
+}
+
+#[derive(Debug)]
+pub struct HashScalar {
+    hasher: VarBlake2b,
+}
+
+impl HashScalar {
+    pub fn new() -> Self {
+        Self {
+            hasher: VarBlake2b::new(<Scalar as HashToField>::InputLength::USIZE)
+                .expect("Invalid hasher output size"),
+        }
+    }
+
+    #[inline]
+    pub fn digest(input: impl AsRef<[u8]>) -> Scalar {
+        let mut state = Self::new();
+        state.update(input.as_ref());
+        state.finalize()
+    }
+
+    pub fn update(&mut self, input: impl AsRef<[u8]>) {
+        self.hasher.update(input.as_ref());
+    }
+
+    pub fn finalize(self) -> Scalar {
         let mut buf = GenericArray::default();
-        hash_q.finalize_variable(|hash| {
+        self.hasher.finalize_variable(|hash| {
             buf.copy_from_slice(hash);
         });
         Scalar::from_okm(&buf)
-    }
-
-    pub fn verify(&self, q: Scalar, member: Scalar) -> bool {
-        let gens = Generators::new(&self.pk, &self.h);
-        self.sig.verify(&gens, q, self.timestamp, &self.accum)
-            && check_witness(member, &self.witness, &self.accum, &self.h)
     }
 }
 
@@ -244,6 +438,17 @@ fn main() {
     let start = Instant::now();
     assert!(token.verify(q, members[0]));
     println!("verify token: {:?}", start.elapsed());
+
+    let start = Instant::now();
+    let prepared = token.prepare_proof(q, members[0]);
+    println!("prepare proof: {:?}", start.elapsed());
+    let c = prepared.create_challenge();
+    let proof = prepared.complete(c);
+    let start = Instant::now();
+    let c2 = proof.create_challenge(&gens, c, timestamp);
+    assert!(c == c2);
+    assert!(proof.verify(&pk, &h));
+    println!("verify proof: {:?}", start.elapsed());
 }
 
 #[test]
