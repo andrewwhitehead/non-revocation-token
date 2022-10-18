e2875af0da181ac9c4fdea170579796e7c051ad9
Andrew Whitehead <cywolf@gmail.com>
2022-10-18 13:34:03 -0700

    small cleanup
    Signed-off-by: Andrew Whitehead <cywolf@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,3 @@
-// use core::ops::Deref;
-
-// use bls12_381::{
-//     hash_to_curve::{ExpandMsgXof, HashToCurve},
-//     pairing, G1Affine, G1Projective, G2Affine, G2Projective, Scalar,
-// };
-// use ff::Field;
-// use group::{Curve, Group, Wnaf};
-// use rand::{CryptoRng, Rng};
-// use sha3::{
-//     digest::{ExtendableOutput, Update, XofReader},
-//     Sha3XofReader, Shake256,
-// };
-// use subtle::{Choice, ConstantTimeEq, CtOption};
-
 mod common;
 pub use self::common::{HashScalar, HashScalarRead};
 
